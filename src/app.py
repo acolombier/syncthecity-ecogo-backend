0aebed76bcfab13061b5abb95b833a06cbfea6ab
Antoine C <hi@antoinecolombier.fr>
2019-11-15 18:16:26 +0000

    Adding facked tickets and ticket endpoint

--- src/app.py
+++ src/app.py
@@ -1,15 +1,19 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+import random
+
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from search.SearchService import SearchService
+
+import data as stub
 
 
 app = Flask(__name__)
 app.config.update(
     JSON_AS_ASCII=False,
     DEBUG=True,
-    JSONIFY_PRETTYPRINT_REGULAR=False,
+    JSONIFY_PRETTYPRINT_REGULAR=False
 )
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
@@ -19,6 +23,36 @@ headers = {'Content-Type': 'application/json; charset=utf-8'}
 @app.route('/', methods=['GET'])
 def home():
     return jsonify({'status': 'OK'}), 200, headers
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    data = request.json
+    searchService = SearchService()
+    if 'from' not in data or 'to' not in data:
+        return jsonify(
+            {
+                'error': {
+                    'code': 400,
+                    'message': 'Invalid Request, please include from and to values in post body'
+                }
+            }
+        ), 400, headers
+
+    print(data)
+    # Search for Options
+    results = searchService.search(data['from']['lat'], data['from']['lon'], data['to']['lat'], data['to']['lon'])
+    return jsonify(results, 200, headers)
+
+
+@app.route('/ticket', methods=['POST'])
+def ticket():
+    data = request.json
+    return jsonify(
+        {
+            'step': random.choice(stub.tickets)
+        }
+    ), 200, headers
 
 
 @app.route('/search', methods=['POST'])
